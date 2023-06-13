3e9914d59c140c1146615051435bb1b24e2649d3
MattB17 <matt.buckley1755@gmail.com>
2023-06-12 21:27:39 -0400

    Adding some LetFlow routing methods

--- src/letflow-routing/model/ipv4-letflow-routing.cc
+++ src/letflow-routing/model/ipv4-letflow-routing.cc
@@ -12,4 +12,30 @@
 
 namespace ns3 {
 
+NS_LOG_COMPONENT_DEFINE("Ipv4LetFlowRouting");
+
+NS_OBJECT_ENSURE_REGISTERED(Ipv4LetFlowRouting);
+
+// Set the flowlet timeout to 50 microseconds.
+Ipv4LetFlowRouting::Ipv4LetFlowRouting(): m_flowletTimeout(MicroSeconds(50)),
+                                          m_ipv4(0) {
+    NS_LOG_FUNCTION(this);
+}
+
+Ipv4LetFlowRouting::~Ipv4LetFlowRouting() {
+	NS_LOG_FUNCTION(this);
+}
+
+TypeId Ipv4LetFlowRouting::GetTypeId(void) {
+	static TypeId tid = TypeId("ns3::Ipv4LetFlowRouting")
+	    .SetParent<Object>()
+	    .SetGroupName("Internet");
+	    //.AddConstructor<Ipv4LetFlowRouting>();
+	return tid;
+}
+
+void Ipv4LetFlowRouting::SetFlowletTimeout(Time timeout) {
+	m_flowletTimeout = timeout;
+}
+
 }  // namespace ns3
--- src/letflow-routing/model/ipv4-letflow-routing.h
+++ src/letflow-routing/model/ipv4-letflow-routing.h
@@ -13,6 +13,40 @@
 
 namespace ns3 {
 
+struct LetFlowFlowlet {
+	uint32_t port;
+	Time activeTime;
+};
+
+struct LetFlowRouteEntry {
+	Ipv4Address network;
+	Ipv4Mask networkMask;
+	uint32_t port;
+};
+
+// This LetFlow routing class is implemented in each switch.
+class Ipv4LetFlowRouting {
+public:
+	Ipv4LetFlowRouting();
+	~Ipv4LetFlowRouting();
+
+	static TypeId GetTypeId(void);
+
+	void SetFlowletTimeout(Time timeout);
+
+private:
+	// Flowlet timeout (minimum inter-packet gap denoting different flowlets).
+	Time m_flowletTimeout;
+
+	// Ipv4 address associated with this router.
+	Ptr<Ipv4> m_ipv4;
+
+	// Flowlet table.
+	std::map<uint32_t, LetFlowFlowlet> m_flowletTable;
+
+	std::vector<LetFlowRouteEntry> m_routeEntryList;
+};
+
 }  // namespace ns3
 
 #endif  // IPV4_LETFLOW_ROUTING_H
