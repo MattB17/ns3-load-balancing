e430dcf6a1fb7ba962282dc8ebf879e72e3053a3
MattB17 <matt.buckley1755@gmail.com>
2023-06-13 21:49:29 -0400

    Adding LetFlowRoutingHelper

--- src/letflow-routing/helper/ipv4-letflow-routing-helper.h
+++ src/letflow-routing/helper/ipv4-letflow-routing-helper.h
@@ -7,6 +7,18 @@
 
 namespace ns3 {
 
+class Ipv4LetFlowRoutingHelper : public Ipv4RoutingHelper {
+public:
+	Ipv4LetFlowRoutingHelper();
+	Ipv4LetFlowRoutingHelper(const Ipv4LetFlowRoutingHelper&);
+
+	Ipv4LetFlowRoutingHelper* Copy(void) const;
+
+	virtual Ptr<Ipv4RoutingProtocol> Create(Ptr<Node> node) const;
+
+	Ptr<Ipv4LetFlowRouting> GetLetFlowRouting(Ptr<Ipv4> ipv4) const;
+};
+
 }  // namespace ns3
 
 #endif  // IPV4_LETFLOW_ROUTING_HELPER_H
