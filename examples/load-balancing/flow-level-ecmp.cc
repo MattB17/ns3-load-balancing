48dfd728609f8d7ad4a7ccd12fe0924db9195e21
MattB17 <matt.buckley1755@gmail.com>
2023-06-18 08:50:56 -0400

    Adding applications for ECMP

--- examples/load-balancing/flow-level-ecmp.cc
+++ examples/load-balancing/flow-level-ecmp.cc
@@ -25,6 +25,7 @@
 #include "ns3/internet-module.h"
 #include "ns3/point-to-point-module.h"
 #include "ns3/applications-module.h"
+#include "ns3/flow-monitor-helper.h"
 #include "ns3/ipv4-global-routing-helper.h"
 
 // Default Network Topology
@@ -112,7 +113,54 @@ int main(int argc, char* argv[]) {
     ipv4.SetBase("10.1.3.0", "255.255.255.0");
     iic = ipv4.Assign(ndc);
 
-    Simulator::Stop(Seconds(11));
+    // Initialize routing database and set up the routing tables in the nodes.
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+
+    // Create the OnOff application to send UDP datagrams of size 210 bytes at
+    // a rate of 448 Kb/s.
+    uint16_t port = 9;  // Discard port (RFC 863).
+    // Destined to the last node.
+    OnOffHelper onoff("ns3::TcpSocketFactory", Address(
+        InetSocketAddress(iic.GetAddress(1), port)));
+    onoff.SetConstantRate(DataRate("448kb/s"));
+    // Install application on node 0.
+    ApplicationContainer apps = onoff.Install(n.Get(0));
+    apps.Start(Seconds(1.0));
+    apps.Stop(Seconds(10.0));
+
+    // Create a packet sink to receive the packets.
+    // Accepts a connection from any IP address on that port.
+    PacketSinkHelper sink("ns3::TcpSocketFactory", Address(
+        InetSocketAddress(Ipv4Address::GetAny(), port)));
+    apps = sink.Install(n.Get(numNodesInCenter + 2));
+    apps.Start(Seconds(1.0));
+    apps.Stop(Seconds(10.0));
+
+    // A second application sending at a rate of 667 Kb/s.
+    uint16_t port2 =  18;
+    OnOffHelper onoff2("ns3::TcpSocketFactory", Address(
+        InetSocketAddress(iic.GetAddress(1), port2)));
+    onoff2.SetConstantRate(DataRate("667kb/s"));
+    apps = onoff2.Install(n.Get(0));
+    apps.Start(Seconds(2.0));
+    apps.Stop(Seconds(10.0));
+
+    PacketSinkHelper sink2("ns3::TcpSocketFactory", Address(
+        InetSocketAddress(Ipv4Address::GetAny(), port2)));
+    apps = sink2.Install(n.Get(numNodesInCenter + 2));
+    apps.Start(Seconds(2.0));
+    apps.Stop(Seconds(10.0));
+
+    AsciiTraceHelper ascii;
+    p2p.EnableAsciiAll(ascii.CreateFileStream("flow-level-ecmp.tr"));
+    p2p.EnablePcapAll("flow-level-ecmp");
+
+    FlowMonitorHelper flowmonHelper;
+    flowmonHelper.InstallAll();
+
+    flowmonHelper.SerializeToXmlFile("flow-level-ecmp.flowmon", false, false);
+
+    Simulator::Stop(Seconds(11.0));
     Simulator::Run();
     Simulator::Destroy();
 
