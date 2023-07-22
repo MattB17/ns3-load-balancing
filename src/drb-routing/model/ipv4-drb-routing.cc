41bb354b882d17265ae2279d3567676c5dc57098
MattB17 <matt.buckley1755@gmail.com>
2023-07-22 12:37:59 -0400

    Adding path selection functionality for DRB/Weighted Presto

--- src/drb-routing/model/ipv4-drb-routing.cc
+++ src/drb-routing/model/ipv4-drb-routing.cc
@@ -27,9 +27,65 @@ bool Ipv4DrbRouting::AddPath(uint32_t weight, uint32_t path) {
 
     // push back `weight` instances of path.
 	for (uint32_t i = 0; i < weight; i++) {
-		m_paths.push_back(path);
+		m_allPaths.push_back(path);
 	}
 	return true;
 }
+
+
+ bool Ipv4DrbRouting::AddWeightedPathToDsts(
+ 	uint32_t weight, uint32_t path,
+    const std::set<Ipv4Address>& dstIPs) {
+    // First we add path to the list of all paths.
+    Ipv4DrbRouting::AddPath(weight, path);
+
+    // Add rules to all other tables
+    std::set<Ipv4Address>::iterator itr = dstIPs.begin();
+    for (; itr != dstIPs.end(); itr++) {
+    	// We start from an empty list of paths, if we don't currently know
+    	// of any paths to the destination we will just add the current path.
+    	std::vector<uint32_t> paths;
+    	auto dstPathPair = m_dstPaths.find(*itr);
+
+    	// If we already have a list of paths for this destination, start
+    	// from there.
+    	if (dstPathPair != m_dstPaths.end()) {
+    		paths = dstPathPair->second; 
+    	}
+
+    	// Now we add the path to the list.
+    	for (uint32_t i = 0; i < weight; i++) {
+    		paths.push_back(path);
+    	}
+    	m_dstPaths[*itr] = paths;
+    }
+    return true;
+ }
+
+ bool Ipv4DrbRouting::AddWeightedPathToDst(
+ 	Ipv4Address dstAddr, uint32_t weight, uint32_t path) {
+ 	// First we add path to the list of all paths.
+ 	Ipv4DrbRouting::AddPath(weight, path);
+
+ 	// Now add rule to the destination paths. We start from an empty list of
+ 	// paths, if we don't currently know of any paths to the destination then
+ 	// we will just add the current path.
+ 	std::vector<uint32_t> paths;
+ 	auto dstPathPair = m_dstPaths.find(dstAddr);
+
+ 	// If we already have a list of paths for this destination, start
+ 	// from there.
+ 	if (dstPathPair != m_dstPaths.end()) {
+ 		paths = dstPathPair->second;
+ 	}
+
+ 	// Now we add the path to the list.
+ 	for (uint32_t i = 0; i < weight; i++) {
+ 		paths.push_back(path);
+ 	}
+ 	m_dstPaths[dstAddr] = paths;
+
+ 	return true;
+ }
 
 }  // namespace ns3
--- src/drb-routing/model/ipv4-drb-routing.h
+++ src/drb-routing/model/ipv4-drb-routing.h
@@ -2,6 +2,11 @@
 #define IPV4_DRB_ROUTING_H
 
 #include "ns3/object.h"
+#include "ns3/ipv4-header.h"
+#include "ns3/ipv4-address.h"
+
+#include <map>
+#include <set>
 
 namespace ns3
 {
@@ -27,8 +32,21 @@ public:
     bool AddPath(uint32_t path);
     bool AddPath(uint32_t weight, uint32_t path);
 
+    bool AddWeightedPathToDsts(
+        uint32_t weight, uint32_t path,
+        const std::set<Ipv4Address>& dstIPs = std::set<Ipv4Address>());
+
+    bool AddWeightedPathToDst(
+        Ipv4Address dstAddr, uint32_t weight, uint32_t path);
+
 private:
-    std::vector<uint32_t> m_paths;
+    // Default paths to use when a path to the destination is not known.
+    // Corresponds to just randomly selecting a path in hopes that a future
+    // hop will know the path to the destination.
+    std::vector<uint32_t> m_allPaths;
+    // Destination specific paths. For each destination, a list of weighted
+    // paths is maintained to that destination.
+    std::map<Ipv4Address, std::vector<uint32_t>> m_dstPaths;
     enum DrbRoutingMode m_mode;
 };
 
