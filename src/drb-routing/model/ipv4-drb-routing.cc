c9e5849d73f37bb6252c4cfa832b862c502ecee6
MattB17 <matt.buckley1755@gmail.com>
2023-07-22 16:05:36 -0400

    Integrating DRB with Ipv4RoutingProtocol

--- src/drb-routing/model/ipv4-drb-routing.cc
+++ src/drb-routing/model/ipv4-drb-routing.cc
@@ -1,5 +1,12 @@
 #include "ipv4-drb-routing.h"
 #include "ns3/log.h"
+#include "ns3/simulator.h"
+#include "ns3/net-device.h"
+#include "ns3/channel.h"
+#include "ns3/node.h"
+#include "ns3/flow-id-tag.h"
+//#include "ns3/ipv4-xpath-tag.h"
+#include "ns3/uinteger.h"
 
 namespace ns3
 {
@@ -33,8 +40,8 @@ bool Ipv4DrbRouting::AddPath(uint32_t weight, uint32_t path) {
 }
 
 
- bool Ipv4DrbRouting::AddWeightedPathToDsts(
- 	uint32_t weight, uint32_t path,
+bool Ipv4DrbRouting::AddWeightedPathToDsts(
+	uint32_t weight, uint32_t path,
     const std::set<Ipv4Address>& dstIPs) {
     // First we add path to the list of all paths.
     Ipv4DrbRouting::AddPath(weight, path);
@@ -60,9 +67,9 @@ bool Ipv4DrbRouting::AddPath(uint32_t weight, uint32_t path) {
     	m_dstPaths[*itr] = paths;
     }
     return true;
- }
+}
 
- bool Ipv4DrbRouting::AddWeightedPathToDst(
+bool Ipv4DrbRouting::AddWeightedPathToDst(
  	Ipv4Address dstAddr, uint32_t weight, uint32_t path) {
  	// First we add path to the list of all paths.
  	Ipv4DrbRouting::AddPath(weight, path);
@@ -86,6 +93,104 @@ bool Ipv4DrbRouting::AddPath(uint32_t weight, uint32_t path) {
  	m_dstPaths[dstAddr] = paths;
 
  	return true;
- }
+}
+
+// In DRB, the RouteOutput method will not actually route the packets out
+// but will instead assign the path ID. DRB relies on list routing and static
+// routing to do the actual forwarding.
+Ptr<Ipv4Route> Ipv4DrbRouting::RouteOutput(
+ 	Ptr<Packet> p, const Ipv4Header& header, Ptr<NetDevice> oif,
+ 	Socket::SocketErrno& sockerr) {
+ 	if (p == 0) {
+ 		return 0;
+ 	}
+
+ 	uint32_t flowIdentity = 0;
+ 	if (m_mode == PER_FLOW) {
+ 		FlowIdTag flowIdTag;
+ 		bool found = p->PeekPacketTag(flowIdTag);
+ 		if (!found) {
+ 			sockerr = Socket::ERROR_NOROUTETOHOST;
+ 			return 0;
+ 		}
+ 		flowIdentity = flowIdTag.GetFlowId();
+ 		NS_LOG_LOGIC("For flow with flow id: " << flowIdentity);
+ 	}
+ 	// m_mode == PER_DEST
+ 	else {
+ 		// header.GetDestination() returns a Ipv4Address. Calling Get() on the
+ 		// result gives a uint32_t.
+ 		flowIdentity = header.GetDestination().Get();
+ 		NS_LOG_LOGIC("For flow with dest: " << flowIdentity);
+ 	}
+
+ 	std::vector<uint32_t> paths;
+ 	auto dstItr = m_dstPaths.find(header.GetDestination());
+ 	if (dstItr == m_dstPaths.end()) {
+ 		// If we can't find the destination, just pick a random path.
+ 		paths = m_allPaths;
+ 	} else {
+ 		// Otherwise use the set of paths to the destination.
+ 		paths = dstItr->second;
+ 	}
+
+ 	uint32_t pathIdx;
+ 	auto flowItr = m_flowPathMap.find(flowIdentity);
+ 	if (flowItr != m_flowPathMap.end()) {
+ 		// If we already have a path for the flow, send it there.
+ 		pathIdx = flowItr->second;
+ 	} else {
+ 		// Otherwise, pick a random path.
+ 		pathIdx = rand() % paths.size();
+ 	}
+
+ 	// DRB sends the packets of a flow (or to a destination) in a round robin
+ 	// fashion. So we send the packet down the current path and increment the
+ 	// path index by 1 for the next packet.
+ 	uint32_t path = paths[pathIdx];
+ 	m_flowPathMap[flowIdentity] = (pathIdx + 1) % paths.size();
+
+    // DRB doesn't actually do the sending, just tags the packet with the
+    // path.
+ 	//Ipv4XPathTag ipv4XPathTag;
+ 	//ipv4XPathTag.SetPathId(path)
+ 	//p->AddPacketTag(ipv4XPathTag);
+
+ 	NS_LOG_LOGIC("DRB Routing has assigned path: " << path);
+
+ 	sockerr = Socket::ERROR_NOTERROR;
+
+ 	return 0;
+}
+
+bool Ipv4DrbRouting::RouteInput(
+ 	Ptr<const Packet> p, const Ipv4Header& header, Ptr<const NetDevice> idev,
+ 	UnicastForwardCallback usb, MulticastForwardCallback mcb,
+ 	LocalDeliverCallback lcb, ErrorCallback ecb) {
+ 	NS_LOG_ERROR("DRB can only support end host routing");
+ 	ecb(p, header, Socket::ERROR_NOROUTETOHOST);
+ 	return false;
+}
+
+void Ipv4DrbRouting::NotifyInterfaceUp(uint32_t interface) {}
+
+void Ipv4DrbRouting::NotifyInterfaceDown(uint32_t interface) {}
+
+void Ipv4DrbRouting::NotifyAddAddress(
+ 	uint32_t interface, Ipv4InterfaceAddress address) {}
+
+void Ipv4DrbRouting::NotifyRemoveAddress(
+	uint32_t interface, Ipv4InterfaceAddress address) {}
+
+void Ipv4DrbRouting::SetIpv4(Ptr<Ipv4> ipv4) {
+	NS_LOG_LOGIC(this << " Setting up Ipv4: " << ipv4);
+ 	NS_ASSERT(m_ipv4 == 0 && ipv4 != 0);
+ 	m_ipv4 = ipv4;
+}
+
+void Ipv4DrbRouting::PrintRoutingTable(
+	Ptr<OutputStreamWrapper> stream, Time::Unit unit) const {}
+
+void Ipv4DrbRouting::DoDispose() {}
 
 }  // namespace ns3
--- src/drb-routing/model/ipv4-drb-routing.h
+++ src/drb-routing/model/ipv4-drb-routing.h
@@ -1,7 +1,10 @@
 #ifndef IPV4_DRB_ROUTING_H
 #define IPV4_DRB_ROUTING_H
 
+#include "ns3/ipv4-routing-protocol.h"
+#include "ns3/ipv4-route.h"
 #include "ns3/object.h"
+#include "ns3/packet.h"
 #include "ns3/ipv4-header.h"
 #include "ns3/ipv4-address.h"
 
@@ -24,7 +27,7 @@ enum DrbRoutingMode {
  * "Per-packet Load-balanced, Low-Latency Routing for Clos-based Data Center
  * Networks"
  */
-class Ipv4DrbRouting {
+class Ipv4DrbRouting : public Ipv4RoutingProtocol {
 public:
     Ipv4DrbRouting();
     ~Ipv4DrbRouting();
@@ -39,6 +42,27 @@ public:
     bool AddWeightedPathToDst(
         Ipv4Address dstAddr, uint32_t weight, uint32_t path);
 
+    // Inherited from Ipv4RoutingProtocol
+    virtual Ptr<Ipv4Route> RouteOutput(
+        Ptr<Packet> p, const Ipv4Header& header, Ptr<NetDevice> oif,
+        Socket::SocketErrno& sockerr);
+    virtual bool RouteInput(
+        Ptr<const Packet> p, const Ipv4Header& header,
+        Ptr<const NetDevice> idev, UnicastForwardCallback ucb,
+        MulticastForwardCallback mcb, LocalDeliverCallback lcb,
+        ErrorCallback ecb);
+    virtual void NotifyInterfaceUp(uint32_t interface);
+    virtual void NotifyInterfaceDown(uint32_t interface);
+    virtual void NotifyAddAddress(
+        uint32_t interface, Ipv4InterfaceAddress address);
+    virtual void NotifyRemoveAddress(
+        uint32_t interface, Ipv4InterfaceAddress address);
+    virtual void SetIpv4(Ptr<Ipv4> ipv4);
+    virtual void PrintRoutingTable(
+        Ptr<OutputStreamWrapper> stream, Time::Unit unit = Time::S) const;
+
+    virtual void DoDispose();
+
 private:
     // Default paths to use when a path to the destination is not known.
     // Corresponds to just randomly selecting a path in hopes that a future
@@ -47,7 +71,12 @@ private:
     // Destination specific paths. For each destination, a list of weighted
     // paths is maintained to that destination.
     std::map<Ipv4Address, std::vector<uint32_t>> m_dstPaths;
+    // Maintains a map of flow IDs to paths. So if a flow already has an
+    // associated path we can just reuse that path.
+    std::map<uint32_t, uint32_t> m_flowPathMap;
     enum DrbRoutingMode m_mode;
+
+    Ptr<Ipv4> m_ipv4;
 };
 
 
