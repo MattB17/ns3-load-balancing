0a978004a764da3ddb95f228d8af8cdf6dd2c712
MattB17 <matt.buckley1755@gmail.com>
2023-07-23 09:17:11 -0400

    Adding Ipv4DrbRoutingHelper

--- src/drb-routing/helper/ipv4-drb-routing-helper.cc
+++ src/drb-routing/helper/ipv4-drb-routing-helper.cc
@@ -1,8 +1,49 @@
 #include "ipv4-drb-routing-helper.h"
+#include "ns3/log.h"
 
 namespace ns3
 {
 
-/* ... */
+NS_LOG_COMPONENT_DEFINE("Ipv4DrbRoutingHelper");
 
+Ipv4DrbRoutingHelper::Ipv4DrbRoutingHelper() {}
+
+Ipv4DrbRoutingHelper::Ipv4DrbRoutingHelper(const Ipv4DrbRoutingHelper&) {}
+
+Ipv4DrbRoutingHelper* Ipv4DrbRoutingHelper::Copy() const {
+	return new Ipv4DrbRoutingHelper(*this);
 }
+
+Ptr<Ipv4RoutingProtocol> Ipv4DrbRoutingHelper::Create(Ptr<Node> node) const {
+	Ptr<Ipv4DrbRouting> drbRouting = CreateObject<Ipv4DrbRouting>();
+	return drbRouting;
+}
+
+Ptr<Ipv4DrbRouting>
+Ipv4DrbRoutingHelper::GetDrbRouting(Ptr<Ipv4> ipv4) const {
+	Ptr<Ipv4RoutingProtocol> ipv4rp = ipv4->GetRoutingProtocol();
+	// If the routing protocol is a vanilla Ipv4DrbRouting object then just
+	// return it.
+	if (DynamicCast<Ipv4DrbRouting>(ipv4rp)) {
+		return DynamicCast<Ipv4DrbRouting>(ipv4rp);
+	}
+	// If instead the routing protocol is a list routing protocol then parse
+	// out the Ipv4DrbRouting protocol (if it's in the list) and return it.
+	if (DynamicCast<Ipv4ListRouting>(ipv4rp)) {
+		Ptr<Ipv4ListRouting> lrp = DynamicCast<Ipv4ListRouting>(ipv4rp);
+		int16_t priority;
+		// Loop through all the routing protocols, getting the protocol and
+		// its corresponding priority.
+		for (uint32_t i = 0; i < lrp->GetNRoutingProtocols(); i++) {
+			Ptr<Ipv4RoutingProtocol> temp = lrp->GetRoutingProtocol(i, priority);
+			if (DynamicCast<Ipv4DrbRouting>(temp)) {
+				return DynamicCast<Ipv4DrbRouting>(temp);
+			}
+		}
+	}
+	// Otherwise, the routing protocol does not have a Ipv4DrbRouting protocol
+	// so just return a null ptr.
+	return 0;
+}
+
+}  // namespace ns3
--- src/drb-routing/helper/ipv4-drb-routing-helper.h
+++ src/drb-routing/helper/ipv4-drb-routing-helper.h
@@ -2,14 +2,28 @@
 #define IPV4_DRB_ROUTING_HELPER_H
 
 #include "ns3/ipv4-drb-routing.h"
+#include "ns3/ipv4-routing-helper.h"
 
 namespace ns3
 {
 
-// Each class should be documented using Doxygen,
-// and have an \ingroup drb-routing directive
+/**
+ * \ingroup drb-routing
+ * \brief a helper to manage DRB routing.
+ * 
+ * A helper to manage an instance of `Ipv4DrbRouting`.
+ */
+class Ipv4DrbRoutingHelper : public Ipv4RoutingHelper {
+public:
+	Ipv4DrbRoutingHelper();
+	Ipv4DrbRoutingHelper(const Ipv4DrbRoutingHelper&);
 
-/* ... */
+	Ipv4DrbRoutingHelper* Copy() const;
+
+	virtual Ptr<Ipv4RoutingProtocol> Create(Ptr<Node> node) const;
+
+	Ptr<Ipv4DrbRouting> GetDrbRouting(Ptr<Ipv4> ipv4) const;
+};
 
 }  // namespace ns3
 
