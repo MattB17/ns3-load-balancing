dd18db85ec3852ec2b62163c80e833d7730a48f5
MattB17 <matt.buckley1755@gmail.com>
2023-08-12 00:02:54 -0400

    Adding EcmpFlowRoutingHelper

--- src/ecmp-flow-routing/helper/ipv4-ecmp-flow-routing-helper.cc
+++ src/ecmp-flow-routing/helper/ipv4-ecmp-flow-routing-helper.cc
@@ -1,8 +1,75 @@
 #include "ipv4-ecmp-flow-routing-helper.h"
 
+#include "ns3/global-router-interface.h"
+#include "ns3/ipv4-global-routing.h"
+#include "ns3/log.h"
+
 namespace ns3
 {
 
-/* ... */
+NS_LOG_COMPONENT_DEFINE("Ipv4EcmpFlowRoutingHelper");
+
+Ipv4EcmpFlowRoutingHelper::Ipv4EcmpFlowRoutingHelper() {}
+
+Ipv4EcmpFlowRoutingHelper::Ipv4EcmpFlowRoutingHelper(
+	const Ipv4EcmpFlowRoutingHelper& o) {
 
 }
+
+Ipv4EcmpFlowRoutingHelper* Ipv4EcmpFlowRoutingHelper::Copy() const {
+	return new Ipv4EcmpFlowRoutingHelper(*this);
+}
+
+Ptr<Ipv4RoutingProtocol>
+Ipv4EcmpFlowRoutingHelper::Create(Ptr<Node> node) const {
+	NS_LOG_LOGIC("Adding GlobalRouter interface to node " << node->GetId());
+	Ptr<GlobalRouter> globalRouter = CreateObject<GlobalRouter>();
+	node->AggregateObject(globalRouter);
+
+	NS_LOG_LOGIC("Adding GlobalRouting interface " << node->GetId());
+	Ptr<Ipv4GlobalRouting> globalRouting = CreateObject<Ipv4GlobalRouting>();
+	globalRouter->SetRoutingProtocol(globalRouting);
+
+	Ptr<Ipv4EcmpFlowRouting> ecmpFlowRouting =
+	    CreateObject<Ipv4EcmpFlowRouting>(globalRouting);
+	return ecmpFlowRouting;
+}
+
+void Ipv4EcmpFlowRoutingHelper::PopulateRoutingTables() {
+	GlobalRouteManager::BuildGlobalRoutingDatabase();
+	GlobalRouteManager::InitializeRoutes();
+}
+
+void Ipv4EcmpFlowRoutingHelper::RecomputeRoutingTables() {
+	GlobalRouteManager::DeleteGlobalRoutes();
+	GlobalRouteManager::BuildGlobalRoutingDatabase();
+	GlobalRouteManager::InitializeRoutes();
+}
+
+Ptr<Ipv4EcmpFlowRouting>
+Ipv4EcmpFlowRoutingHelper::GetEcmpFlowRouting(Ptr<Ipv4> ipv4) const {
+	Ptr<Ipv4RoutingProtocol> ipv4rp = ipv4->GetRoutingProtocol();
+	// If the routing protocol can be cast to Ipv4EcmpFlowRouting then return
+	// the cast.
+	if (DynamicCast<Ipv4EcmpFlowRouting>(ipv4rp)) {
+		return DynamicCast<Ipv4EcmpFlowRouting>(ipv4rp);
+	}
+	// If the routing protocol can be cast to Ipv4ListRouting then perform the
+	// cast and iterate through the list searching for an Ipv4EcmpFlowRouting
+	// object.
+	if (DynamicCast<Ipv4ListRouting>(ipv4rp)) {
+		Ptr<Ipv4ListRouting> lrp = DynamicCast<Ipv4ListRouting>(ipv4rp);
+		int16_t priority;
+		for (uint32_t route_idx = 0;
+			route_idx < lrp->GetNRoutingProtocols();
+			route_idx++) {
+			Ptr<Ipv4RoutingProtocol> temp = lrp->GetRoutingProtocol(route_idx, priority);
+			if (DynamicCast<Ipv4EcmpFlowRouting>(temp)) {
+				return DynamicCast<Ipv4EcmpFlowRouting>(temp);
+			}
+		}
+	}
+	return nullptr;
+}
+
+}  // namespace ns3
--- src/ecmp-flow-routing/helper/ipv4-ecmp-flow-routing-helper.h
+++ src/ecmp-flow-routing/helper/ipv4-ecmp-flow-routing-helper.h
@@ -2,15 +2,76 @@
 #define ECMP_FLOW_ROUTING_HELPER_H
 
 #include "ns3/ipv4-ecmp-flow-routing.h"
+#include "ns3/ipv4-routing-helper.h"
+#include "ns3/node-container.h"
 
 namespace ns3
 {
 
-// Each class should be documented using Doxygen,
-// and have an \ingroup ecmp-flow-routing directive
+/**
+ * \ingroup ipv4EcmpFlowHelper
+ * 
+ * \brief Helper class that adds ns3::Ipv4EcmpFlowRouting objects.
+ */
+class Ipv4EcmpFlowRoutingHelper : public Ipv4RoutingHelper {
+public:
+	/**
+	 * \brief Construct a Ipv4EcmpFlowRoutingHelper to more easily manage ECMP
+	 * flow-level routing.
+	 */
+	Ipv4EcmpFlowRoutingHelper();
 
-/* ... */
+	/**
+	 * \brief Construct a Ipv4EcmpFlowRoutingHelper from another previously
+	 * initialized instance (Copy Constructor).
+	 * 
+	 * \param o Ipv4EcmpFlowRoutingHelper object to be copied.
+	 */
+	Ipv4EcmpFlowRoutingHelper(const Ipv4EcmpFlowRoutingHelper& o);
 
-}
+	// Delete assignment operator to avoid misuse.
+	Ipv4EcmpFlowRoutingHelper& operator=(
+		const Ipv4EcmpFlowRoutingHelper&) = delete;
 
-#endif /* ECMP_FLOW_ROUTING_HELPER_H */
+	/**
+	 * \return pointer to close of this Ipv4EcmpFlowRoutingHelper.
+	 */
+	Ipv4EcmpFlowRoutingHelper* Copy() const override;
+
+	/**
+	 * \brief This method is called by ns3::InternetStackHelper::Install.
+	 * 
+	 * \param node The node on which the routing protocol will run.
+	 * 
+	 * \returns a newly-created routing protocol.
+	 */
+	Ptr<Ipv4RoutingProtocol> Create(Ptr<Node> node) const override;
+
+	/**
+	 * \brief Build a routing database and initialize the routing tables of
+	 * the nodes in the simulation. Makes all nodes in the simulation into
+	 * routers.
+	 */
+	static void PopulateRoutingTables();
+
+	/**
+	 * \brief Remove all routes that were previously installed in a prior call
+	 * to either PopulateRoutingTables() or RecomputeRoutingTables(), and add
+	 * a new set of routes.
+	 */
+	static void RecomputeRoutingTables();
+
+	/**
+	 * \brief Retrieve the Ipv4EcmpFlowRouting protocol attached to the
+	 * helper.
+	 * 
+	 * \param ipv4 The Ptr<Ipv4> used to search for the routing protocol.
+	 * 
+	 * \returns Ipv4EcmpFlowRouting pointer or nullptr if not found.
+	 */
+	Ptr<Ipv4EcmpFlowRouting> GetEcmpFlowRouting(Ptr<Ipv4> ipv4) const;
+};
+
+}  // namespace ns3
+
+#endif  // ECMP_FLOW_ROUTING_HELPER_H
