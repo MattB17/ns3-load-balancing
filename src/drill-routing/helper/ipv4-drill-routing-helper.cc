9841a55a4dbf559bb6adfc2e4aaab32185914c98
MattB17 <matt.buckley1755@gmail.com>
2023-08-21 23:06:14 -0400

    Adding DrillRoutingHelper

--- src/drill-routing/helper/ipv4-drill-routing-helper.cc
+++ src/drill-routing/helper/ipv4-drill-routing-helper.cc
@@ -1,8 +1,76 @@
 #include "ipv4-drill-routing-helper.h"
 
+#include "ns3/global-router-interface.h"
+#include "ns3/ipv4-global-routing.h"
+#include "ns3/log.h"
+
 namespace ns3
 {
 
-/* ... */
+NS_LOG_COMPONENT_DEFINE("Ipv4DrillRoutingHelper");
+
+Ipv4DrillRoutingHelper::Ipv4DrillRoutingHelper() {}
+
+Ipv4DrillRoutingHelper::Ipv4DrillRoutingHelper(
+		const Ipv4DrillRoutingHelper& o) {
+}
+
+Ipv4DrillRoutingHelper* Ipv4DrillRoutingHelper::Copy() const {
+	return new Ipv4DrillRoutingHelper(*this);
+}
+
+Ptr<Ipv4RoutingProtocol> Ipv4DrillRoutingHelper::Create(Ptr<Node> node) const {
+	// Add the global router interface because this is what is used to run
+	// the routing algorithm and construct routes.
+	NS_LOG_LOGIC("Adding GlobalRouter interface to node " << node->GetId());
+	Ptr<GlobalRouter> globalRouter = CreateObject<GlobalRouter>();
+	node->AggregateObject(globalRouter);
+
+	NS_LOG_LOGIC("Adding GlobalRouting interface " << node->GetId());
+	Ptr<Ipv4GlobalRouting> globalRouting = CreateObject<Ipv4GlobalRouting>();
+	globalRouter->SetRoutingProtocol(globalRouting);
+
+	Ptr<Ipv4DrillRouting> drillRouting = CreateObject<Ipv4DrillRouting>(
+			globalRouting);
+	return drillRouting;
+}
+
+void Ipv4DrillRoutingHelper::PopulateRoutingTables() {
+	GlobalRouteManager::BuildGlobalRoutingDatabase();
+	GlobalRouteManager::InitializeRoutes();
+}
+
+void Ipv4DrillRoutingHelper::RecomputeRoutingTables() {
+	GlobalRouteManager::DeleteGlobalRoutes();
+	GlobalRouteManager::BuildGlobalRoutingDatabase();
+	GlobalRouteManager::InitializeRoutes();
+}
+
+Ptr<Ipv4DrillRouting>
+Ipv4DrillRoutingHelper::GetDrillRouting(Ptr<Ipv4> ipv4) const {
+	Ptr<Ipv4RoutingProtocol> ipv4rp = ipv4->GetRoutingProtocol();
+	// If the routing protocol can be cast to Ipv4DrillRouting then return
+	// the cast.
+    if (DynamicCast<Ipv4DrillRouting>(ipv4rp)) {
+		return DynamicCast<Ipv4DrillRouting>(ipv4rp);
+	}
+	// If the routing protocol can be cast to Ipv4ListRouting then perform the
+	// cast and iterate through the list searching for an Ipv4DrillRouting
+	// object.
+	if (DynamicCast<Ipv4ListRouting>(ipv4rp)) {
+		Ptr<Ipv4ListRouting> lrp = DynamicCast<Ipv4ListRouting>(ipv4rp);
+		int16_t priority;
+		for (uint32_t route_idx = 0;
+			 route_idx < lrp->GetNRoutingProtocols();
+			 route_idx++) {
+			Ptr<Ipv4RoutingProtocol> temp = lrp->GetRoutingProtocol(
+					route_idx, priority);
+			if (DynamicCast<Ipv4DrillRouting>(temp)) {
+				return DynamicCast<Ipv4DrillRouting>(temp);
+			}
+		}
+	}
+	return nullptr;
+}
 
 }  // namespace ns3
--- src/drill-routing/helper/ipv4-drill-routing-helper.h
+++ src/drill-routing/helper/ipv4-drill-routing-helper.h
@@ -2,14 +2,66 @@
 #define IPV4_DRILL_ROUTING_HELPER_H
 
 #include "ns3/ipv4-drill-routing.h"
+#include "ns3/ipv4-routing-helper.h"
+#include "ns3/node-container.h"
 
 namespace ns3
 {
 
-// Each class should be documented using Doxygen,
-// and have an \ingroup drill-routing directive
+class Ipv4DrillRoutingHelper : public Ipv4RoutingHelper {
+	/**
+	 * \brief Construct a DrillRoutingHelper to more easily manage DRILL
+	 * routing.
+	 */
+	Ipv4DrillRoutingHelper();
 
-/* ... */
+	/**
+	 * \brief Construct a DrillRoutingHelper from another previously
+	 * initialized instance (Copy Constructor).
+	 *
+	 * \param o Object to be copied.
+	 */
+	Ipv4DrillRoutingHelper(const Ipv4DrillRoutingHelper& o);
+
+	// Delete assignment operator to avoid misuse.
+	Ipv4DrillRoutingHelper& operator=(const Ipv4DrillRoutingHelper&) = delete;
+
+	/**
+	 * \return pointer to close of this Ipv4DrillRoutingHelper.
+	 */
+	Ipv4DrillRoutingHelper* Copy() const override;
+
+	/**
+	 * \brief This method is called by ns3::InternetStackHelper::Install.
+	 *
+	 * \param node The node on which the routing protocol will run.
+	 *
+	 * \returns a newly-created routing protocol.
+	 */
+	virtual Ptr<Ipv4RoutingProtocol> Create(Ptr<Node> node) const override;
+
+	/**
+	 * \brief Build a routing database and initialize the routing tables of the
+	 * nodes in the simulation. Makes all nodes in the simulation into routers.
+	 */
+	static void PopulateRoutingTables();
+
+	/**
+	 * \brief Remove all routes that were previously installed in a prior call
+	 * to either PopulateRoutingTables() or RecomputeRoutingTables(), and add
+	 * a new set of routes.
+	 */
+	static void RecomputeRoutingTables();
+
+	/**
+	 * \brief Retrieve the Ipv4DrillRouting protocol attached to the helper.
+	 *
+	 * \param ipv4 The Ptr<Ipv4> to search for the DRILL routing protocol.
+	 *
+	 * \returns Ipv4DrillRouting pointer or nullptr if not found.
+	 */
+	Ptr<Ipv4DrillRouting> GetDrillRouting(Ptr<Ipv4> ipv4) const;
+};
 
 }  // namespace ns3
 
